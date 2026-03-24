91e8474b46d6f5864700e4fded356512381f0d98
Emmanuel Jr <46963516+masterjayr@users.noreply.github.com>
2026-03-24 17:01:04 +0100

    added ios realtimeclassifcication project

---
+++ RealtimeImageClassifieriOS/RealtimeClassificationiOS/RealtimeClassificationiOS/CameraPreview.swift
@@ -0,0 +1,31 @@
+//
+//  CameraPreview.swift
+//  RealtimeClassificationiOS
+//
+//  Created by Emmanuel Emmanuel on 23/03/2026.
+//
+import SwiftUI
+import AVFoundation
+
+struct CameraPreview: UIViewRepresentable {
+    let session: AVCaptureSession
+    
+    func makeUIView(context: Context) -> PreviewView {
+        let view = PreviewView()
+        view.videoPreviewLayer.session = session
+        view.videoPreviewLayer.videoGravity = .resizeAspectFill
+        return view
+    }
+    
+    func updateUIView(_ uiView: PreviewView, context: Context){}
+}
+
+final class PreviewView: UIView {
+    override class var layerClass: AnyClass {
+        AVCaptureVideoPreviewLayer.self
+    }
+    
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        layer as! AVCaptureVideoPreviewLayer
+    }
+}
---
+++ RealtimeImageClassifieriOS/RealtimeClassificationiOS/RealtimeClassificationiOS/RealtimeClassificationiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealtimeClassificationiOSApp.swift
+//  RealtimeClassificationiOS
+//
+//  Created by Emmanuel Emmanuel on 23/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct RealtimeClassificationiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
